a1c0ff97fc9383b7b7ca3862e90636f4f54afbd9
Mukesh Prajapat <darwin.mukesh2022@gmail.com>
2023-08-24 12:51:19 +0530

    productlisting

--- src/pages/productlist.jsx
+++ src/pages/productlist.jsx
@@ -1,15 +1,60 @@
-import Layout from "../Components/layout/layout"
+import Layout from "../Components/layout/layout";
 import Header from "../Components/header/header";
 import Footer from "../Components/footer/footer";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setProduct,removeProduct} from "../redux/actions/action";
+import "./productlist.css";
 
-const ProductList = ()=>{
-    return <>
-    <Header/>
-    <Layout>
-        <h1>productlist</h1>
-    </Layout>
-    <Footer/>
+const ProductList = () => {
+  //take state from store
+  const product = useSelector((state) => state);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products").then(
+          (res) => res.json()
+        );
+        dispatch(setProduct(response));
+      } catch (err) {
+        console.log("err", err.message);
+      }
+    }
+    fetchData();
+    return () => {dispatch(removeProduct())}
+  }, []);
+  return (
+    <>
+      <Header />
+      <Layout>
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4  g-4">
+          {product.Allproduct.lenght !== 0 ? (
+            product.Allproduct.map((element, index) => (
+              <div className="col" key={index}>
+                <div className="card" style={{height:"400px"}}>
+                <div className="card-img" style={{backgroundImage:`url(${element.image})`}}/>
+                  <div className="card-body">
+                    <h5 className="card-title">{element.title}</h5>
+                    <p className="card-text">
+                      <span>
+                        Price:<span>{element.price}</span>
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <h1>loading....</h1>
+          )}
+        </div>
+      </Layout>
+      <Footer />
     </>
-}
+  );
+};
 
-export default ProductList
+export default ProductList;
---
+++ src/redux/actions/action.jsx
@@ -0,0 +1,22 @@
+
+export const setProduct = (product)=>{
+    return {
+        type:"SET_PRODUCT",
+        payload:product
+    }
+}
+
+export const selectProduct = (product)=>{
+    return {
+        type:"SELECT_PRODUCT",
+        payload:product
+    }
+}
+
+export const removeProduct = ()=>{
+    return {
+        type:"REMOVE_PRODUCT",
+    }
+}
+
+
